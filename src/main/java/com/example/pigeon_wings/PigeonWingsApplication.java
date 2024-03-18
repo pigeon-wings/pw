300a706c3448e37178f74705c7b5b8f9d2150198
pigeon_wings <145859097+pigeon-wings@users.noreply.github.com>
2024-03-18 21:19:01 +0600

    Initial commit of pigeon_wings

---
+++ src/main/java/com/example/pigeon_wings/PigeonWingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pigeon_wings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonWingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonWingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pigeon_wings/PigeonWingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pigeon_wings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonWingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
